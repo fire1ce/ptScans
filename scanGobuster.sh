2fb4e10fe967d6078c439c1982c0fd800b433194
fire1ce <stas@3os.org>
2019-12-08 13:53:15 +0200

    fix

--- scanGobuster.sh
+++ scanGobuster.sh
@@ -2,15 +2,13 @@
 export PATH=/opt/bin:/usr/local/bin:/usr/contrib/bin:/bin:/usr/bin:/usr/sbin:/usr/bin/X11
 cd $(dirname "$(realpath "$0")")
 
-DATE=`date +%y%m%d%I%M`
+DATE=$(date +%y%m%d%I%M)
 URL=$1
-DomainName=`echo "${URL}" | sed -e 's|^[^/]*//||' -e 's|/.*$||' -e 's|:.*$||'`
+DomainName=$(echo "${URL}" | sed -e 's|^[^/]*//||' -e 's|/.*$||' -e 's|:.*$||')
 
-
-if [ $# -eq 0 ]
-then
+if [ $# -eq 0 ]; then
     echo "Provide a URL to Scan"
     exit 1
 fi
 
-nikto -Display V -o ./results/${DATE}.Nikto.${DomainName}.html -Format html -h ${URL}
+gobuster dir -w /usr/share/dirb/wordlists/big.txt -u ${URL} -o ./results/${DATE}.gobuster.${DomainName}.log
