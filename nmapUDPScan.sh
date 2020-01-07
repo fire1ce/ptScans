bc35293392e7df2ee5da58514e51d56d4f44d444
fire1ce <stas@3os.org>
2020-01-07 11:50:18 +0200

    dev

--- nmapUDPScan.sh
+++ nmapUDPScan.sh
@@ -2,19 +2,15 @@
 export PATH=/opt/bin:/usr/local/bin:/usr/contrib/bin:/bin:/usr/bin:/usr/sbin:/usr/bin/X11
 cd $(dirname "$(realpath "$0")")
 
-DATE=`date +%y%m%d%I%M`
+DATE=$(date +%y%m%d%I%M)
 URL=$1
-DomainName=`echo "${URL}" | sed -e 's|^[^/]*//||' -e 's|/.*$||' -e 's|:.*$||'`
+DomainName=$(echo "${URL}" | sed -e 's|^[^/]*//||' -e 's|/.*$||' -e 's|:.*$||')
 
-
-if [ $# -eq 0 ]
-then
+if [ $# -eq 0 ]; then
     echo "Provide a URL to Scan"
     exit 1
 fi
 
 nmap -sU -sV -p 1-65535 -T4 -oX ./results/${DATE}.nmap.${DomainName}.xml --script ssl-enum-ciphers ${DomainName} &&
-xsltproc ./results/${DATE}.nmap.${DomainName}.xml -o ./results/${DATE}.nmap.${DomainName}.html && sleep 2 &&
-rm -rf ./results/${DATE}.nmap.${DomainName}.xml
-
-
+    xsltproc ./results/*.nmap.${DomainName}.xml -o ./results/${DATE}.nmap.${DomainName}.html && sleep 2 &&
+    rm -rf ./results/${DATE}.nmap.${DomainName}.xml
