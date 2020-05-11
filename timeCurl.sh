a5626fb78cdf58ce6b306901d2f3fd89d8b05b62
fire1ce <stas@3os.org>
2020-05-11 12:54:00 +0300

    update to timecurl

--- timeCurl.sh
+++ timeCurl.sh
@@ -6,5 +6,5 @@ URL=$1
 
 while true; do
     sleep 1
-    curl -o /dev/null -s -w 'Total: %{time_total}s ' ${URL} ; echo ${URL}
+    curl -s -w 'Testing Website Response Time for :%{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nAppCon Time:\t\t%{time_appconnect}\nRedirect Time:\t\t%{time_redirect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null ${URL}
 done
