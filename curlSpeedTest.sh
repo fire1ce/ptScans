d296a3fec11c550192fcceeda4dab9f0f68effbc
fire1ce <stas@3os.org>
2020-05-11 12:57:47 +0300

    update to timecurl

--- curlSpeedTest.sh
+++ curlSpeedTest.sh
@@ -4,6 +4,11 @@ export PATH=/opt/bin:/usr/local/bin:/usr/contrib/bin:/bin:/usr/bin:/usr/sbin:/us
 # Params
 URL=$1
 
+if [ $# -eq 0 ]; then
+    echo "Provide a URL"
+    exit 1
+fi
+
 while true; do
     sleep 1
     curl -s -w 'Testing Website Response Time for :%{url_effective}\n\nLookup Time:\t\t%{time_namelookup}\nConnect Time:\t\t%{time_connect}\nAppCon Time:\t\t%{time_appconnect}\nRedirect Time:\t\t%{time_redirect}\nPre-transfer Time:\t%{time_pretransfer}\nStart-transfer Time:\t%{time_starttransfer}\n\nTotal Time:\t\t%{time_total}\n' -o /dev/null ${URL}
--- test clearURL.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export PATH=/opt/bin:/usr/local/bin:/usr/contrib/bin:/bin:/usr/bin:/usr/sbin:/usr/bin/X11
-URL=$1
-DATE=$(date +%y%m%d%I%M)
-
-DomainName=$(echo "${URL}" | sed -e 's|^[^/]*//||' -e 's|/.*$||' -e 's|:.*$||')
-
-echo ${DomainName}
